3c8d0b4055c9f2edb05ac06ea14a8decab3d8247
adomingu <alfonso.dominguez-de-la-iglesia-external@capgemini.com>
2021-06-10 13:09:20 +0200

    moving more things to device

--- convolutional_layer.py
+++ convolutional_layer.py
@@ -89,9 +89,9 @@ class Convolutional_layer():
                 del w_r_coeffs
 
                 convolutional_matrix.to(self.device), vector_matrix.to(self.device)
-                final_matrix = torch.matmul(convolutional_matrix, vector_matrix)
+                final_matrix = torch.matmul(convolutional_matrix, vector_matrix).to(self.device)
                 del vector_matrix
-                final_vector = torch.sum(final_matrix, 0)
+                final_vector = torch.sum(final_matrix, 0).to(self.device)
                 final_vector = torch.squeeze(final_vector, 1)
 
                 # When all the "weighted vectors" are added, we add on the b_conv.
@@ -137,7 +137,7 @@ class Convolutional_layer():
             vectors.append(child.vector)
 
         # We create a matrix with all the vectors
-        vector_matrix = torch.stack(tuple(vectors), 0)
+        vector_matrix = torch.stack(tuple(vectors), 0).to(self.device)
         del vectors
         # We create a tensor with the parameters associated to the top matrix
         w_t_params = torch.tensor(w_t_list).to(self.device)
